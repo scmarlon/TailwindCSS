e0071b723cae03511c7811a21fc2b67cc54fdbbb
MARLON SÁNCHEZ CRUZ <marsc_97@hotmail.com>
2024-05-15 17:58:30 -0600

    set everything up

---
+++ tailwind-css/src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+export default function Home() {
+  return (
+    <>
+      <h1 className="font-bold underline text-red-500 text-3xl">
+        Hello Marlon
+      </h1>
+    </>
+  );
+}
